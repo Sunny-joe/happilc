6d1cbb7491cdb8092ffc9bca912ce934be1df63c
Sunny-joe <90485916+Sunny-joe@users.noreply.github.com>
2021-10-07 04:44:45 +0100

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+
+const mongoCon = (url)=>{
+    mongoose.connect(url)
+}
+
+
+module.exports = mongoCon
---
+++ index.js
@@ -0,0 +1,39 @@
+
+const express = require('express')
+const mongoCon = require('./connect/connect')
+const router = require('./routes/products')
+require('dotenv').config()
+
+const app = express()
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+
+app.use('/api/v1/products', router)
+
+
+
+app.use(express.static('public'))
+
+
+ const port = process.env.PORT || 5000
+
+const start = async()=>{
+    try {
+        await mongoCon(process.env.MONGO_URI)
+       
+        app.listen(port, ()=>console.log('connected'))
+
+    } catch (e) {
+        console.log(e)
+        
+    }
+}
+
+start()
+
+
+// /storage/emulated/0/SunnyProject/node-project   
